b0fc48e4ffa7d470304152ac476f94dd4f458fb3
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-04-29 11:22:55 -0700

    fix: add all FTEs given we are all remote participants now
    Also added Claudio to snacks given that he is still with us.

--- R/PEPmeeting.R
+++ R/PEPmeeting.R
@@ -12,8 +12,8 @@
 #' @examples PEPmeeting(20180919, notattending = c("Ian"), snacks = c("Nis", "Owen"))
 #' 
 PEPmeeting <- function(date, notattending = NA,
-  leads = c("Brian", "Chantel", "Jason", "John", "Kelli", "Kristin", "Ian", "Melissa", "Vlada"),
-  snacks = c("Josh", "Owen", "Christine", "Kathryn")) {
+  leads = c("Aaron", "Andi", "Brian", "Chantel", "Jason", "John", "Kelli", "Kristin", "Ian", "Melissa", "Vlada"),
+  snacks = c("Owen", "Christine", "Claudio", "Kathryn")) {
 
   # Alternatively, calculate the seed based on N days after today
   #N <- 7
